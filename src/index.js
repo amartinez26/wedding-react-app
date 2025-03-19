b9e06394687575f212a8cde8ab3ca93f7e3917c9
amartinez26 <tun26tun@msn.com>
2025-03-19 17:40:29 -0500

    adding css, html and changing it to work in react code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-function Greeting() {
-  return <h2>wedding app boilerplate</h2>
-}
+import './index.css'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(
+  <App />
+);
