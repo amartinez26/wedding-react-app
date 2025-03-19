4b46c0370101c0970c74fdd07ebec1e9fd75f83c
amartinez26 <tun26tun@msn.com>
2025-03-19 15:35:40 -0500

    boiler plate first commit

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <>Wedding app</>
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function Greeting() {
+  return <h2>wedding app boilerplate</h2>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<Greeting />)
