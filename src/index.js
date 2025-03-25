e237ea0d6b5bd6a6c02f5ff06280c4b43e060ef2
amartinez26 <tun26tun@msn.com>
2025-03-25 18:20:25 -0500

    importing AOS and adding css

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-// import './assets/css/main.css';
-// import './assets/vendor/bootstrap/css/bootstrap.min.css';
-// import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
-// import './assets/vendor/aos/aos.css';
-// import './assets/vendor/glightbox/css/glightbox.min.css';
-// import './assets/vendor/swiper/swiper-bundle.min.css';
+import './assets/vendor/bootstrap/css/bootstrap.min.css';
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
+import './assets/vendor/aos/aos.css';
+import './assets/vendor/glightbox/css/glightbox.min.css';
+import './assets/vendor/swiper/swiper-bundle.min.css';
+// Import AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Ensure AOS CSS is imported
+import './assets/css/main.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
+AOS.init();
 root.render(
   <App />
 );
